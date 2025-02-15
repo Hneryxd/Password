d4836587ecb1c0541b98b918a7e887e72832d112
Hneryxd <davidllerena1124@gmail.com>
2025-02-15 09:29:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-caract = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-longitud = int(input('Digite la longitud de la contraseña'))
-password = ''
-for i in range(longitud):
-    password += random.choice(caract)
-print (password)# Password
+import random #importa la variable random, para generar o escojer de forma aleatoria
+caract = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" #todas los digitos disponibles para la contraseña
+longitud = int(input('Digite la longitud de la contraseña')) #longitud de la contraseña 
+password = '' #variable vacia, se llenara con la longitud de caracteres y los caracteres escogidos aleatoriamente
+for i in range(longitud): #repetira determinadamente la accion
+    password += random.choice(caract) #escoje de forma aleatoria digitos de la variable caract para completar la contraseña
+print (password) # Password
